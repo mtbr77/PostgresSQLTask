48b07671428c16cc667215fdfcbcc4caed3b4ea7
Viacheslav <mtbr77@rambler.ru>
2019-07-23 18:18:17 +0300

    Add files via upload

---
+++ src/main/java/com/task/postgresql/CellidApplication.java
@@ -0,0 +1,13 @@
+package com.task.postgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CellidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CellidApplication.class, args);
+	}
+
+}
